cbee4833efb7757c3a4d1eaec9af0c8708ea74d0
dinasv <dinasv@post.bgu.ac.il>
2019-01-22 16:16:45 +0200

    Add tests: suffix tree, algorithm comparison

---
+++ src/Test/java/CompareAlgorithmsTest.java
@@ -0,0 +1,52 @@
+import Core.AlgorithmType;
+import Core.CSBFinderWorkflow;
+import Core.Genomes.GenomesInfo;
+import Core.Parameters;
+import Core.Patterns.Pattern;
+import Core.PostProcess.Family;
+import IO.Parsers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ */
+public class CompareAlgorithmsTest {
+
+    private final String GENOMES_FILE_PATH = this.getClass().getResource("/genomes.fasta").getPath();
+    private final String REF_GENOMES_FILE_PATH = this.getClass().getResource("/ref_genomes.txt").getPath();
+
+    @Test
+    public void testRefGenomesEqualOutput() throws Exception {
+        Parameters params = new Parameters();
+        params.maxInsertion = 1;
+
+        GenomesInfo gi = Parsers.parseGenomesFile(GENOMES_FILE_PATH);
+        CSBFinderWorkflow workflow = new CSBFinderWorkflow(gi);
+
+        List<Pattern> patternsFromFile = readPatternsReferenceGenomesFile(gi);
+        workflow.setPatternsFromFile(patternsFromFile);
+
+        workflow.setAlgorithm(AlgorithmType.SUFFIX_TREE.algorithm);
+        List<Family> familiesAlg1 = workflow.run(params);
+
+        gi.initAlphabet();
+        workflow.setAlgorithm(AlgorithmType.MATCH_POINTS.algorithm);
+        List<Family> familiesAlg2 = workflow.run(params);
+
+        Assert.assertEquals(familiesAlg1.size(), familiesAlg2.size());
+    }
+
+    private List<Pattern> readPatternsReferenceGenomesFile(GenomesInfo genomesInfo) throws Exception{
+        List<Pattern> patterns = new ArrayList<>();
+        String path = REF_GENOMES_FILE_PATH;
+
+        if (path != null) {
+
+            patterns = Parsers.parseReferenceGenomesFile(genomesInfo, path);
+        }
+        return patterns;
+    }
+}
---
+++ src/Test/java/DatasetSuffixTreeTest.java
@@ -0,0 +1,114 @@
+import Core.Genomes.Strand;
+import Core.Genomes.WordArray;
+import Core.Patterns.InstanceLocation;
+import Core.SuffixTreePatternFinder.SuffixTrees.GeneralizedSuffixTree;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ */
+
+public class DatasetSuffixTreeTest {
+
+    int GENOME_ID = 1;
+    int REPLICON_ID = 1;
+
+    private Pair<WordArray, InstanceLocation> wordLocationPair(int[] word){
+        InstanceLocation LOCATION1 = location(word);
+        return new Pair<>(wordArray(word), LOCATION1);
+    }
+
+    private InstanceLocation location(int[] word){
+        return new InstanceLocation(REPLICON_ID, GENOME_ID, 0,
+                word.length, Strand.FORWARD, 0, word.length);
+    }
+
+    private WordArray wordArray(int[] word){
+        return new WordArray(word);
+    }
+
+    private GeneralizedSuffixTree createGstWithWords(List<Pair<WordArray, InstanceLocation>> words){
+        GeneralizedSuffixTree gst = new GeneralizedSuffixTree();
+
+        for (Pair<WordArray, InstanceLocation> word : words) {
+            gst.put(word.getFirst(), word.getSecond());
+        }
+        gst.computeCount();
+
+        return gst;
+    }
+
+    @Test
+    public void testGSTFindSingleWordSubstrings() throws Exception {
+
+        int[] WORD1 = {1, 2, 3, 4};
+        Pair<WordArray, InstanceLocation> WORD_LOC1 = wordLocationPair(WORD1);
+
+        List<Pair<WordArray, InstanceLocation>> words = new ArrayList<>();
+        words.add(WORD_LOC1);
+        GeneralizedSuffixTree gst = createGstWithWords(words);
+
+        for (int startIndex = 0; startIndex < WORD1.length; startIndex++) {
+            for (int endIndex = startIndex+1; endIndex < WORD1.length+1; endIndex++) {
+
+                WordArray WORD_ARRAY1 = wordArray(Arrays.copyOfRange(WORD1, startIndex, endIndex));
+                Map<Integer, List<InstanceLocation>> instances = gst.search(WORD_ARRAY1);
+
+                Assert.assertEquals(1, instances.size());
+                Assert.assertEquals(true, instances.containsKey(WORD_LOC1.getSecond().getGenomeId()));
+
+                InstanceLocation loc = instances.get(WORD_LOC1.getSecond().getGenomeId()).get(0);
+                Assert.assertEquals(startIndex, loc.getActualStartIndex());
+            }
+        }
+    }
+
+    @Test
+    public void testGSTFindSingleRepeatedSubstring() throws Exception {
+        int[] WORD2 = {1, 2, 1, 2};
+        Pair<WordArray, InstanceLocation> WORD_LOC2 = wordLocationPair(WORD2);
+
+        List<Pair<WordArray, InstanceLocation>> words = new ArrayList<>();
+        words.add(WORD_LOC2);
+        GeneralizedSuffixTree gst = createGstWithWords(words);
+
+        WordArray WORD_SUBSTRING = wordArray(Arrays.copyOfRange(WORD2, 0, 2));
+        Map<Integer, List<InstanceLocation>> instances = gst.search(WORD_SUBSTRING);
+
+        Set<Integer> startIndexes = instances.get(GENOME_ID).stream()
+                .map(InstanceLocation::getActualStartIndex)
+                .collect(Collectors.toSet());
+
+        Set<Integer> expectedStartIndexes = new HashSet<>();
+        expectedStartIndexes.add(0);
+        expectedStartIndexes.add(2);
+
+        Assert.assertEquals(expectedStartIndexes, startIndexes);
+
+    }
+
+    /**
+     * A private class used to return a tuples of two elements
+     */
+    private class Pair<A, B> {
+
+        private final A first;
+        private final B second;
+
+        public Pair(A first, B second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public A getFirst() {
+            return first;
+        }
+
+        public B getSecond() {
+            return second;
+        }
+    }
+}
