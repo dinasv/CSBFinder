c7f23bd2a2614dc802043043978a8d601d8b7ee2
dinasv <dinasv@post.bgu.ac.il>
2020-02-02 12:21:51 +0200

    Add patterns from file circular test

--- src/Test/java/PatternsOptionTest.java
+++ src/Test/java/PatternsOptionTest.java
@@ -10,6 +10,7 @@ import model.postprocess.Family;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,11 +22,12 @@ public class PatternsOptionTest {
     private final String GENOMES_FILE_PATH2 = this.getClass().getResource("/genomes2.fasta").getPath();
     private final String PATTERNS_FILE_PATH = this.getClass().getResource("/patterns.fasta").getPath();
     private final String PATTERNS_FILE_PATH2 = this.getClass().getResource("/patterns2.fasta").getPath();
+    private final String PATTERNS_FILE_PATH3 = this.getClass().getResource("/patterns5.fasta").getPath();
 
 
     private List<Pattern> runWorkflow(String genomesFile, List<Pattern> patternsFromFile, Parameters params) throws Exception{
 
-        GenomesInfo gi = Parsers.parseGenomesFile(genomesFile, false);
+        GenomesInfo gi = Parsers.parseGenomesFile(genomesFile, params.circular);
 
         CSBFinderWorkflow workflow = new CSBFinderWorkflow(gi);
 
@@ -78,6 +80,33 @@ public class PatternsOptionTest {
         Assert.assertEquals(expectedPatterns.size(), patterns.size());
         Assert.assertEquals(expectedPatterns, patterns);
         Assert.assertEquals(patternsFromFile.get(0).getPatternId(), patterns.get(0).getPatternId());
+
+    }
+
+    @Test
+    public void testCircularMP() throws Exception {
+        List<Pattern> patternsFromFile = Parsers.parsePatternsFile(PATTERNS_FILE_PATH3);
+        Parameters parameters = new Parameters();
+        parameters.algorithmType = AlgorithmType.MATCH_POINTS;
+        parameters.circular = true;
+        parameters.maxInsertion = 1;
+
+        List<Pattern> patterns = runWorkflow(GENOMES_FILE_PATH1, patternsFromFile, parameters);
+
+        Gene[] expectedGenes = {new Gene("COG0003", Strand.INVALID),
+                new Gene("COG0004", Strand.INVALID),
+                new Gene("COG0001", Strand.INVALID)};
+
+        Pattern expectedPattern = new Pattern(expectedGenes);
+        expectedPattern.setPatternId(patternsFromFile.get(0).getPatternId());
+
+        Assert.assertEquals(1, patterns.size());
+
+        Pattern pattern = patterns.get(0);
+
+        Assert.assertEquals(expectedPattern, pattern);
+        Assert.assertEquals(expectedPattern.getPatternId(), pattern.getPatternId());
+        Assert.assertEquals(2, pattern.getInstancesPerGenomeCount());
 
     }
 }
